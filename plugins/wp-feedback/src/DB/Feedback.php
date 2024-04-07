6d8221fde6eef2833a2243e286755a872ce2e01d
Yousof <50125998+ykamal@users.noreply.github.com>
2024-04-07 16:01:03 +0100

    Update Feedback.php

--- plugins/wp-feedback/src/DB/Feedback.php
+++ plugins/wp-feedback/src/DB/Feedback.php
@@ -5,15 +5,16 @@ namespace WPFB\DB;
 use WPFB\Constants;
 
 /**
- * Handles feedback management
+ * Handles feedback management.
  */
-class Feedback {
-
+class Feedback
+{
     private $table_name;
 
-    public function __construct() {
+    public function __construct()
+    {
         global $wpdb;
-        $this->table_name = $wpdb->prefix . Constants::DB_NAME_FB_TRACKER;;
+        $this->table_name = $wpdb->prefix.Constants::DB_NAME_FB_TRACKER;
     }
 
     /**
@@ -21,9 +22,9 @@ class Feedback {
      *
      * This function inserts feedback data into the database table.
      *
-     * @param int    $post_id      The ID of the post for which feedback is being added.
-     * @param string $ip           The IP address of the user giving the feedback.
-     * @param bool   $is_helpful   (Optional) Whether the feedback is helpful or not. Defaults to true.
+     * @param int    $post_id    The ID of the post for which feedback is being added.
+     * @param string $ip         The IP address of the user giving the feedback.
+     * @param bool   $is_helpful (Optional) Whether the feedback is helpful or not. Defaults to true.
      *
      * @throws \Exception When either post ID or IP address is empty, or if the user has already voted.
      *
@@ -31,20 +32,21 @@ class Feedback {
      */
     public function add_feedback(int $post_id, string $ip, bool $is_helpful = true)
     {
-        if(!$post_id || !$ip) throw new \Exception("add_feedback requires a post ID and IP");
-
-        if($this->has_voted($post_id, $ip)) throw new \Exception("Already voted");
-
+        if (!$post_id || !$ip) {
+            throw new \Exception('add_feedback requires a post ID and IP');
+        }
+        if ($this->has_voted($post_id, $ip)) {
+            throw new \Exception('Already voted');
+        }
         global $wpdb;
 
         $result = $wpdb->insert($this->table_name, [
-            "content_id"    =>  $post_id,
-            "is_helpful"    =>  $is_helpful,
-            "ip"            =>  $ip
+            'content_id' => $post_id,
+            'is_helpful' => $is_helpful,
+            'ip' => $ip,
         ]);
 
         return $result;
-        
     }
 
     /**
@@ -63,8 +65,9 @@ class Feedback {
      */
     public function get_feedback(int $post_id): array
     {
-        if(!$post_id) throw new \Exception("get_feedback requires a post ID");
-
+        if (!$post_id) {
+            throw new \Exception('get_feedback requires a post ID');
+        }
         global $wpdb;
 
         $sql = "SELECT SUM(is_helpful = 1) AS count_helpful, 
@@ -79,38 +82,63 @@ class Feedback {
         );
 
         return $this->calculate_percentages($data);
-        
     }
 
     /**
-     * Calculate percentages based on the data provided
+     * Calculates feedback percentages based on the provided data.
+     *
+     * This function calculates the percentage of helpful and unhelpful feedback based on the provided data.
+     *
+     * @param object $data An object containing feedback data:
+     *                     - 'count_helpful': The count of helpful feedback.
+     *                     - 'count_not_helpful': The count of not helpful feedback.
+     *                     - 'count_total': The total count of feedback.
+     *
+     * @return array An associative array containing feedback percentages:
+     *               - 'total': The total count of feedback.
+     *               - 'helpful': The percentage of helpful feedback.
+     *               - 'unhelpful': The percentage of unhelpful feedback.
      */
     public function calculate_percentages($data): array
     {
-        if(!$data) return [];
+        if (!$data) {
+            return [];
+        }
 
         $total = $data->count_total;
         $total_helpful = ($data->count_helpful < 1 || $total < 1) ? 0 : round(($data->count_helpful / $total) * 100, 0);
         $total_unhelpful = $total < 1 ? 0 : 100 - $total_helpful;
 
         return [
-            'total' =>  $total,
-            'helpful'   =>  $total_helpful,
-            'unhelpful' =>  $total_unhelpful
+            'total' => $total,
+            'helpful' => $total_helpful,
+            'unhelpful' => $total_unhelpful,
         ];
-
     }
 
+    /**
+     * Checks if a user has voted for a specific post.
+     *
+     * This function queries the database to determine if a user with the provided IP address
+     * has voted for the given post ID.
+     *
+     * @param int    $post_id The ID of the post to check for voting.
+     * @param string $ip      The IP address of the user.
+     *
+     * @return bool True if the user has voted, false otherwise.
+     */
     public function has_voted(int $post_id, string $ip): bool
     {
-        if(!$post_id || !$ip) return false;
+        if (!$post_id || !$ip) {
+            return false;
+        }
 
         global $wpdb;
 
         $exists = $wpdb->get_var(
             $wpdb->prepare(
                 "SELECT COUNT(*) from {$this->table_name} WHERE content_id = %d AND ip = %s",
-                $post_id, 
+                $post_id,
                 $ip
             )
         );
@@ -118,10 +146,23 @@ class Feedback {
         return boolval($exists);
     }
 
+    /**
+     * Retrieves a vote record for a post based on IP address.
+     *
+     * This function retrieves the vote record from the database for a given post ID and IP address.
+     *
+     * @param int    $post_id The ID of the post for which to retrieve the vote record.
+     * @param string $ip      The IP address of the user.
+     *
+     * @throws \Exception When the post ID is empty.
+     *
+     * @return object|null The vote record as an object, or null if no vote record found.
+     */
     public function get_vote_by_ip(int $post_id, string $ip)
     {
-        if(!$post_id) throw new \Exception("get_vote_by_ip requires a post ID");
-
+        if (!$post_id) {
+            throw new \Exception('get_vote_by_ip requires a post ID');
+        }
         global $wpdb;
 
         $sql = "SELECT * FROM {$this->table_name} WHERE content_id = %d AND ip = %s LIMIT 1";
@@ -131,7 +172,5 @@ class Feedback {
         );
 
         return $data;
-
     }
-
 }
